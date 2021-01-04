510237581cd3edd450cface7402a39d9aa146243
EvanRPavone <evanrpavone@gmail.com>
2021-01-04 14:38:17 -0500

    commit tree 2c3e74d526a185dde6c1f47ff71530805dcb195a

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+let names = ["Evan", "Shelby", "Rob"];
+let theEvent = ["birthday"];
+
+function writeCards(names, theEvent) {
+    let result = [];
+    for (let i = 0; i < names.length; i++) {
+        result.push(`Thank you, ${names[i]}, for the wonderful ${theEvent} gift!`);
+    }
+    return result;
+}
+writeCards(names, theEvent);
+
+let number = 10;
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
